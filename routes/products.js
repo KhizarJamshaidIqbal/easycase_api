4214646ccbbd2de12076e050a3948ff1e388456b
KhizarJamshaidIqbal <112169672+KhizarJamshaidIqbal@users.noreply.github.com>
2025-03-11 10:52:50 +0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -16,5 +16,4 @@ router.get("/", authenticate, getProducts);
 router.get("/search", authenticate, searchProducts);
 router.put("/:id", authenticate, updateProduct);
 router.delete("/:id", authenticate, deleteProduct);
-router.get("/count", authenticate, getProductCount);
 module.exports = router;
